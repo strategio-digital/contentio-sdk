38a7c9ad299ca929b745ba6ed3f51f98b8547083
Jiří Zapletal <jz@strategio.digital>
2022-02-22 15:33:05 +0100

    Updated copy

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Copyright (c) 2021 Strategio Digital s.r.o.
+ * Copyright (c) 2022 Strategio Digital s.r.o.
  * @author Jiří Zapletal (https://strategio.digital, jz@strategio.digital)
  */
 declare(strict_types=1);
